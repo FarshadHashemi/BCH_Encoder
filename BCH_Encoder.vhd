9d576bba937ea6f728fafab06b0a8ddc9898b4c6
FarshadHashemi <121182056+FarshadHashemi@users.noreply.github.com>
2022-12-30 15:45:48 +0330

    Update BCH_Encoder.vhd

--- BCH_Encoder.vhd
+++ BCH_Encoder.vhd
@@ -4,8 +4,8 @@ Use IEEE.STD_LOGIC_1164.All ;
 Entity BCH_Encoder Is
 
 	Generic(
-		Kbch			:	Integer									:=	12432 ;
-		Nbch			:	Integer									:=	12600 ;
+		K_BCH			:	Integer									:=	12432 ;
+		N_BCH			:	Integer									:=	12600 ;
 		Polynomial	:	STD_Logic_Vector(192 downto 0)	:= "0000000000000000000000001010000000110001011011011111010101001100001101001101100100110001011001101001000111010001110010000011010010101001010001111111001111101011111010001000110010000010110100101"
 	);
 	
@@ -31,14 +31,14 @@ Architecture Behavioral Of BCH_Encoder Is
 	Signal	Valid_Output_Data_Register		:	STD_Logic										:= '0' ;
 	
 --	Generator Or Divisor Or Polynomial
-	Constant	Generator							:	STD_Logic_Vector(Nbch-Kbch-1 Downto 0)	:= Polynomial(Nbch-Kbch-1 Downto 0) ;
+	Constant	Generator							:	STD_Logic_Vector(N_BCH-K_BCH-1 Downto 0)	:= Polynomial(N_BCH-K_BCH-1 Downto 0) ;
 --	%%%%%%%%%%%%%%%%%%%%
 	
 --	Remainder
-	Signal	Remainder							:	STD_Logic_Vector(Nbch-Kbch-1 Downto 0)	:= (Others=>'0') ;
+	Signal	Remainder							:	STD_Logic_Vector(N_BCH-K_BCH-1 Downto 0)	:= (Others=>'0') ;
 --	%%%%%%%%%
 	
-	Signal	Valid_SRL							:	STD_Logic_Vector(Nbch-Kbch-1 Downto 0)	:= (Others=>'0') ;
+	Signal	Valid_SRL							:	STD_Logic_Vector(NbN_BCHch-K_BCH-1 Downto 0)	:= (Others=>'0') ;
 	
 Begin
 	
@@ -66,23 +66,23 @@ Begin
 			Elsif (Clock_Enable_Register='1') Then
 					
 				--	Valid_SRL Is A Shift Register To Specify Valid Output Data
-					Valid_SRL						<=	Valid_SRL(Nbch-Kbch-2 Downto 0) & Valid_Input_Data_Register ;
-					Valid_Output_Data_Register	<=	Valid_SRL(Nbch-Kbch-1) AND (NOT Valid_Input_Data_Register) ;
+					Valid_SRL						<=	Valid_SRL(N_BCH-K_BCH-2 Downto 0) & Valid_Input_Data_Register ;
+					Valid_Output_Data_Register	<=	Valid_SRL(N_BCH-K_BCH-1) AND (NOT Valid_Input_Data_Register) ;
 				--	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 				
 				If Valid_Input_Data_Register='1' Then
 				
 				--	Computing BCH Code
-					Remainder(0)		<=	Remainder(Nbch-Kbch-1) XOR Input_Data_Register ;
-					For i In 1 To (Nbch-Kbch-1) Loop
-						Remainder(i)	<=	(Remainder(Nbch-Kbch-1) AND Generator(i)) XOR Remainder(i-1) ;
+					Remainder(0)		<=	Remainder(N_BCH-K_BCH-1) XOR Input_Data_Register ;
+					For i In 1 To (N_BCH-K_BCH-1) Loop
+						Remainder(i)	<=	(Remainder(N_BCH-K_BCH-1) AND Generator(i)) XOR Remainder(i-1) ;
 					End Loop ;
 				--	%%%%%%%%%%%%%%%%%%
 					
 				Else
 					
-					Remainder				<=	Remainder(Nbch-Kbch-2 Downto 0) & '0' ;
-					Output_Data_Register	<=	Remainder(Nbch-Kbch-1) ;
+					Remainder				<=	Remainder(N_BCH-K_BCH-2 Downto 0) & '0' ;
+					Output_Data_Register	<=	Remainder(N_BCH-K_BCH-1) ;
 					
 				End If ;
 			
